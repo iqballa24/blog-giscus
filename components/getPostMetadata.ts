0ce2bf79a8a51c63c77c2fff4c22c608e97055fa
Iqbal Nugraha <tengku.nugraha@erajaya.com>
2023-12-06 10:26:15 +0700

    blog static next js

---
+++ components/getPostMetadata.ts
@@ -0,0 +1,25 @@
+import matter from 'gray-matter';
+import fs from 'fs';
+import { PostMetadata } from '@/components/PostMetadata';
+
+export const getPostMetadata = (): PostMetadata[] => {
+  const folder = 'posts/';
+  const files = fs.readdirSync(folder);
+
+  const markdownPosts = files.filter((file) => file.endsWith('.md'));
+  const slugs = markdownPosts.map((file) => file.replace('.md', ''));
+
+  const posts = markdownPosts.map((filename) => {
+    const fileContents = fs.readFileSync(`posts/${filename}`, 'utf8');
+    const matterResult = matter(fileContents);
+
+    return {
+      title: matterResult.data.title,
+      date: matterResult.data.date,
+      subtitle: matterResult.data.subtitle,
+      slug: filename.replace('.md', ''),
+    };
+  });
+
+  return posts;
+};
